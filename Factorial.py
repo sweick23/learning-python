f5b1a3999ad779ada45f2a2dfe922573bad9c78b
Shane Weickum <sweick23@outlook.com>
2018-02-24 08:45:12 -0800

    Create Factorial.py
    This program takes an integer as an input like the number 5 and returns the factorial of the input. For 5 the factorial is 120. 
    Which is 1 * 2 * 3 * 4 * 5 = 120

---
+++ Factorial.py
@@ -0,0 +1,17 @@
+def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n -1
+    return num
+        
+print factorial(1)
+print factorial(2)
+print factorial(3)
+print factorial(4)
+print factorial(5)
+print factorial(6)
+print factorial(7)
+print factorial(8)
+print factorial(9)
+print factorial(10)
